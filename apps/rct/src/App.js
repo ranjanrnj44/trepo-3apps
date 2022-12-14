fe996787e4e025ecf25c54ec161398f2eb6b1d12
ranjanrnj44 <ranjanrnj44@gmail.com>
2022-12-14 15:44:22 +0530

    =removed private connect

--- apps/rct/src/App.js
+++ apps/rct/src/App.js
@@ -2,11 +2,9 @@
 // import { Buttons } from "ui";
 import "./App.css";
 //import as commonJS module
-let mathOps = require("@AccentureVFInnovation/helperfunction");
 
 function App() {
   //log the data
-  console.log(mathOps.subtract(6, 3));
   return (
     <div className="App">
       <h1>React Js</h1>
